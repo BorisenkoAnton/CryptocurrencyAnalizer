577d633d9a12fca143059f21841f1b13151370b4
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-08 10:26:09 +0300

    created service to manage networking via AFNetworking

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Network Service/NetworkService.h
@@ -0,0 +1,33 @@
+//
+//  NetworkService.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/7/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+@import AFNetworking;
+
+#ifndef NetworkService_h
+#define NetworkService_h
+
+@interface NetworkService : AFHTTPSessionManager
+
+@property NSURLSessionConfiguration *configuration;
+@property NSString *baseUrl;
+@property NSString *apiKey;
+
++ (id)shared;
+
+- (void)downloadData:(NSString *)url
+          parameters:(nullable id)parameters
+          headers:(nullable NSDictionary<NSString *,NSString *> *)headers
+          completion:(void (^_Nonnull)(NSObject * _Nullable data))completion;
+- (void)getAvailableCoins:(void (^_Nullable)(NSArray * _Nonnull availableCoins))completion;
+- (void)getHistoricalDataForCoin:(NSString *_Nonnull)coin
+                       withLimit:(NSNumber *_Nullable)limit
+                       completion:(void (^_Nullable)(NSArray * _Nullable coinData))completion;
+
+@end
+
+#endif /* NetworkService_h */
---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Network Service/NetworkService.m
@@ -0,0 +1,93 @@
+//
+//  NetworkService.m
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/7/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NetworkService.h"
+
+// Service to manage networking via AFNetworking
+@implementation NetworkService
+
++ (id)shared {
+    
+    static NetworkService *networkService = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        networkService = [[self alloc] init];
+        networkService.configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        networkService.baseUrl = @"https://min-api.cryptocompare.com";
+        networkService.requestSerializer = [AFJSONRequestSerializer serializer];
+    });
+    return networkService;
+}
+
+- (void)downloadData:(NSString *)url
+          parameters:(nullable id)parameters
+          headers:(nullable NSDictionary<NSString *,NSString *> *)headers
+          completion:(void (^)(NSObject *data))completion {
+
+    [self.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    [self GET:url parameters:parameters headers:headers progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        completion(responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error: %@", error);
+        completion(nil);
+    }];
+    
+}
+
+// Getting historical data for needed period and needed coin
+- (void)getHistoricalDataForCoin:(NSString *)coin withLimit:(NSNumber *)limit completion:(void (^)(NSArray *coinData))completion {
+    
+    NSString *url = [self.baseUrl stringByAppendingString:@"/data/v2/histoday"];
+    NSDictionary *body = @{@"fsym":coin, @"tsym":@"USD", @"limit":limit};
+    
+    [self downloadData:url parameters:body headers:nil completion:^(NSObject * _Nullable data) {
+        if ([data isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *response = (NSDictionary *)data;
+            NSDictionary *dataField = [response valueForKey:@"Data"];
+            NSArray *historicalInfo = [dataField valueForKey:@"Data"];
+            
+            NSMutableArray *historicalInfoForCoin = [NSMutableArray new];
+            for (NSObject *historicalInfoItem in historicalInfo) {
+                NSNumber *price = (NSNumber *)[(NSDictionary *)historicalInfoItem valueForKey:@"high"];
+                [historicalInfoForCoin addObject:price];
+            }
+            
+            completion(historicalInfoForCoin);
+        } else {
+            completion(nil);
+        }
+    }];
+}
+
+// Getting array of NSString names of all available coins
+- (void)getAvailableCoins:(void (^)(NSArray *availableCoins))completion {
+    
+    NSString *url = [self.baseUrl stringByAppendingString:@"/data/blockchain/list"];
+    NSDictionary *body = @{@"api_key":@"f2796ad1a4b0f52d336872f658e1d1fd76b0ca2789ee28e89f9b5858fde95461"};
+    
+    [self downloadData:url parameters:body headers:nil completion:^(NSObject * _Nullable data) {
+        if ([data isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *response = (NSDictionary *)data;
+            NSDictionary *dataField = [response valueForKey:@"Data"];
+            NSMutableArray *availableCoins = [NSMutableArray new];
+            
+            for (NSString *coin in dataField) {
+                [availableCoins addObject:coin];
+            }
+            
+            completion(availableCoins);
+        } else {
+            completion(nil);
+        }
+    }];
+}
+
+@end
