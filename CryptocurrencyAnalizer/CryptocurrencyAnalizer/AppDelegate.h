252e6d697afbe80f747c95af0038a210daf41793
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-08 10:25:07 +0300

    initial commit

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/7/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/7/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
