48674785e0ee2dbb7965862a72f74ef63b69f5df
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-21 18:34:14 +0300

    added enum with relative urls

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Support Files/RelativeURLs.h
@@ -0,0 +1,20 @@
+//
+//  RelativeURLs.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/21/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#ifndef RelativeURLs_h
+#define RelativeURLs_h
+
+typedef NS_ENUM(NSInteger, RelativeURL) {
+    RelativeURLDailyHistory,
+    RelativeURLHourlyHistory,
+    RelativeURLMinutelyHistory,
+    RelativeURLAvailableCoins
+};
+
+
+#endif /* RelativeURLs_h */
