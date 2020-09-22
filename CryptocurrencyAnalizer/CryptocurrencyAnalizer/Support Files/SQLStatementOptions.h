42e62e4af79ba161394e9adc9e8d280bdca0057f
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-22 14:56:02 +0300

    relocate Where Condition, Table Column to support files, created new enum with sql options

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Support Files/SQLStatementOptions.h
@@ -0,0 +1,22 @@
+//
+//  SQLStatementOptions.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/22/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#import "WhereCondition.h"
+
+#ifndef SQLStatement_h
+#define SQLStatement_h
+
+typedef struct {
+    NSMutableArray<WhereCondition *> * _Nullable whereConditions;
+    BOOL count;
+    NSString * _Nullable orderBy;
+    BOOL desc;
+    NSString * _Nullable limit;
+} SQLStatementOptions;
+
+#endif /* SQLStatement_h */
