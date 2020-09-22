819024bd732beabae51003c86138fcac0aeb2ec2
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-22 18:01:12 +0300

    added structs with options for Axis Set and Graph

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Support Files/Options structs/GraphOptions.h
@@ -0,0 +1,21 @@
+//
+//  GraphOption.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/22/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#ifndef GraphOption_h
+#define GraphOption_h
+
+typedef struct {
+    CGRect frame;
+    CGColorRef color;
+    CGFloat paddingBottom;
+    CGFloat paddingLeft;
+    CGFloat paddingTop;
+    CGFloat paddingRight;
+} GraphOptions;
+
+#endif /* GraphOption_h */
