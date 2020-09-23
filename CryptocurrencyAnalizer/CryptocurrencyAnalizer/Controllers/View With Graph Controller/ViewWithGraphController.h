0eb938583372f93d8444a80017bcfc4fa3fa2e0c
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-23 13:42:41 +0300

    refactored controller according to created  categories

--- CryptocurrencyAnalizer/CryptocurrencyAnalizer/Controllers/View With Graph Controller/ViewWithGraphController.h
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Controllers/View With Graph Controller/ViewWithGraphController.h
@@ -7,10 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CorePlot-CocoaTouch.h"
+#import "GraphModel.h"
+#import "NetworkService.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ViewWithGraphController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *coinNameTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *periodChoosingSegmentedControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIPickerView *coinNamePickerView;
+@property (weak, nonatomic) IBOutlet CPTGraphHostingView *graphView;
+
+@property NSMutableArray<NSString *> *availableCoins;           // Full list of available coins
+@property NSMutableArray<NSString *> *filteredAvailableCoins;   // List of available coins after searching with the help of text Field
+@property (strong) GraphModel *graphModel;
+@property NetworkService * networkService;
 
 @end
 
