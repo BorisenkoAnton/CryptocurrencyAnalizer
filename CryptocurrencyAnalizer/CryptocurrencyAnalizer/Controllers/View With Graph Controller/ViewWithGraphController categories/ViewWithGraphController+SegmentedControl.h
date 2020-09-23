8ba8fe8143097397db893dd591e0500e072df8e1
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-09-23 13:40:50 +0300

    relocated logic of segmented control actions to separate category

---
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Controllers/View With Graph Controller/ViewWithGraphController categories/ViewWithGraphController+SegmentedControl.h
@@ -0,0 +1,20 @@
+//
+//  ViewWithGraphController+SegmentedControl.h
+//  CryptocurrencyAnalizer
+//
+//  Created by Anton Borisenko on 9/23/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+#import "ViewWithGraphController.h"
+#import "ViewWithGraphController+CacheHandling.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewWithGraphController (SegmentedControl)
+
+- (void)configureSegmentedControl;
+
+@end
+
+NS_ASSUME_NONNULL_END
