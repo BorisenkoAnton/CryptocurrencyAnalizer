88ae1722f472fdda3c062a47cde3592fc96a53c0
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-10-07 14:07:04 +0300

    relocate loading available coins into extension

--- CryptocurrencyAnalizer/CryptocurrencyAnalizer/Controllers/View With Graph Controller/Extensions/ViewWithGraphController+PickerView.m
+++ CryptocurrencyAnalizer/CryptocurrencyAnalizer/Controllers/View With Graph Controller/Extensions/ViewWithGraphController+PickerView.m
@@ -17,24 +17,52 @@
     self.coinNamePickerView.dataSource = dataSource;
 }
 
+
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     
     return 1;
 }
+
 // The number of rows of data
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
     
     return self->filteredAvailableCoins.count;
 }
+
 // The data to return for the row and component (column) that's being passed in
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
     
      return self->filteredAvailableCoins[row];
 }
 
+
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
     
     self.coinNameTextField.text = self->filteredAvailableCoins[row];
+}
+
+
+- (void)loadAvailableCoins {
+    
+    self->availableCoins = [NSMutableArray<NSString *> new];
+    self->networkService = [NetworkManager shared];
+    
+    // Getting list of all available coins, unreasonable to cache it because of the max-age=120
+    [self->networkService getAndParseData:nil withAPILimit:nil completion:^(NSMutableArray<DBModel *> * _Nullable coinData) {
+        for (NSString *coin in coinData) {
+            [self->availableCoins addObject:coin];
+        }
+        
+        [self->availableCoins sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        
+        self->filteredAvailableCoins = [self->availableCoins mutableCopy];
+        
+        [self.coinNamePickerView reloadAllComponents];
+        [self.coinNamePickerView selectRow:0 inComponent:0 animated:NO];
+        
+        [self.activityIndicator stopAnimating];
+    }];
+    
 }
 
 @end
